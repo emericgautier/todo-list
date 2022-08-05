6d2b52586385bce2ff8b0cba568d3e932257d599
emericgautier <emericgtr@gmail.com>
2022-08-05 11:32:18 +0200

    setup files

---
+++ index.js
@@ -0,0 +1,9 @@
+// créer un form pour taper les todos
+
+// Ajouter sur le dom ce qui est tapé dans le form à la validation du form
+
+// Supprimer un todo lorsque l'on click dessus (.remove())
+
+// stocker dans le local storage la liste
+
+// consulter le local storage au lancement de l'application pour rajouter les todos
